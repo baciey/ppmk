9638c6db0152b8d350e482ca4a7c029d72a115de
baciey <baciey@gmail.com>
2020-09-28 22:04:53 +0200

    Updates

---
+++ precache-manifest.b5192361cdb81484643231f1e6fb0a72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02a1ce712b579865c433171c4ea52c8",
+    "url": "/ppmk/index.html"
+  },
+  {
+    "revision": "a811215e90d4b4354a68",
+    "url": "/ppmk/static/css/main.dcd5ffbc.chunk.css"
+  },
+  {
+    "revision": "31716f2f8988982a221d",
+    "url": "/ppmk/static/js/2.45f05a6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ppmk/static/js/2.45f05a6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a811215e90d4b4354a68",
+    "url": "/ppmk/static/js/main.67e65477.chunk.js"
+  },
+  {
+    "revision": "f8ce2f607b7ba7aaa02d",
+    "url": "/ppmk/static/js/runtime-main.96f6e7b0.js"
+  },
+  {
+    "revision": "727276378141c2babd91aa1eaf6aa2f7",
+    "url": "/ppmk/static/media/bgc-img-home.72727637.jpg"
+  },
+  {
+    "revision": "e2e6487f98f49d9557158e0041e74be7",
+    "url": "/ppmk/static/media/logo.e2e6487f.png"
+  }
+]);
